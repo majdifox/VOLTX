c53bbeaf0fabcb73c5a5b66c6be7b0d15351fe6b
majdifox <majmehdi20@gmail.com>
2026-01-03 11:18:00 +0100

    feat: add main application class

---
+++ VoltX_Backend/src/main/java/com/voltx/VoltxApplication.java
@@ -0,0 +1,12 @@
+package com.voltx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoltxApplication.class, args);
+    }
+}
