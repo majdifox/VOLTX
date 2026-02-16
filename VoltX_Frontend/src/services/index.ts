4b342a2f6e6924e67482495281966fd5c653367b
majdifox <majmehdi20@gmail.com>
2026-02-16 09:42:07 +0000

    feat(service): update services index to include notificationService

--- VoltX_Frontend/src/services/index.ts
+++ VoltX_Frontend/src/services/index.ts
@@ -3,4 +3,5 @@ export * from './userService';
 export * from './postService';
 export * from './eventService';
 export * from './commentService';
+export * from './notificationService';
 export { default as api } from './api';
